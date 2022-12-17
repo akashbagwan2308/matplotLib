352524b019e1e0e70b4c65c8736c2e108a7113ee
akashbagwan2308 <akashbagwan01@gmail.com>
2022-12-17 14:22:01 +0530

    Matplotlib tut

--- tut1.py
+++ tut1.py
@@ -9,6 +9,8 @@ y = 2*x*x
 Y= 3*x*x
 z =4*x*x
 print(y)
+print(Y)
+print(z)
 #   Simple plot
 
 # plt.plot(x,y)
@@ -32,10 +34,80 @@ print(y)
 
 # Plot two/multiple graphs in same plane
 
-plt.plot(x,y,color = 'g',linestyle = ':',linewidth = 2)
-plt.plot(x,Y,color = 'red',linestyle = '--',linewidth = 2)
-plt.plot(x,z,color = 'blue',linestyle = '-.',linewidth = 2)
-plt.title("First line plot")
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
-plt.show()
+# plt.plot(x,y,color = 'g',linestyle = ':',linewidth = 2)
+# plt.plot(x,Y,color = 'red',linestyle = '--',linewidth = 2)
+# plt.plot(x,z,color = 'blue',linestyle = '-.',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.show()
+
+#  Plot with grid
+
+# plt.plot(x,y,color = 'g',linestyle = ':',linewidth = 2)
+# plt.plot(x,Y,color = 'red',linestyle = '--',linewidth = 2)
+# plt.plot(x,z,color = 'blue',linestyle = '-.',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.grid(True)   # to show grid
+# plt.show()
+
+#  Sub plots of row 1 ,column 2
+# plt.subplot(1,2,1)
+# plt.plot(x,y,color = 'g',linestyle = '-',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.subplot(1,2,2)
+# plt.plot(x,Y,color = 'red',linestyle = '-.',linewidth = 2)
+# plt.title("Second line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.show()
+
+#  Sub plots of row 2 ,column 1
+# plt.subplot(2,1,1)
+# plt.plot(x,y,color = 'g',linestyle = '-',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.subplot(2,1,2)
+# plt.plot(x,Y,color = 'red',linestyle = '-.',linewidth = 2)
+# plt.title("Second line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.show()
+
+#  Sub plots of row 2 ,column 2
+# plt.subplot(2,2,1)
+# plt.plot(x,y,color = 'g',linestyle = '-',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.subplot(2,2,2)
+# plt.plot(x,Y,color = 'red',linestyle = '-.',linewidth = 2)
+# plt.title("Second line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.subplot(2,2,3)
+# plt.plot(x,y,color = 'g',linestyle = '-',linewidth = 2)
+# plt.title("First line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.subplot(2,2,4)
+# plt.plot(x,Y,color = 'red',linestyle = '-.',linewidth = 2)
+# plt.title("Second line plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+#
+# plt.show()
+
+
