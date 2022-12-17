7e0002710343814503ef2b6d90be568041659842
akashbagwan2308 <akashbagwan01@gmail.com>
2022-12-17 15:09:32 +0530

    Matplotlib tut

---
+++ tut2(Bar Graph).py
@@ -0,0 +1,29 @@
+# Matplotlib ---- Bar Graphs
+from matplotlib import pyplot as plt
+import numpy as np
+
+student = {"Bob": 87,"Matt":56,"Sam":27}
+names = list(student.keys())
+values = list(student.values())
+
+#  simple bar plot
+# plt.bar(names,values)
+# plt.show()
+
+# bar plot with title and labels
+
+# plt.bar(names,values)
+# plt.title("Bar Plot")
+# plt.xlabel("Names")
+# plt.ylabel("Marks")
+# plt.show()
+
+#  Horizontal Bar plot
+
+plt.barh(names,values,color = "green")
+plt.title("Bar Plot")
+plt.xlabel("Marks")
+plt.ylabel("Names")
+plt.grid(True)
+plt.show()
+
