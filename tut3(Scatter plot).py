79e01c2a552b13e1c876b9802e70b2b7b98ca03b
akashbagwan2308 <akashbagwan01@gmail.com>
2022-12-17 15:46:51 +0530

    Matplotlib tut

---
+++ tut3(Scatter plot).py
@@ -0,0 +1,30 @@
+# Scatter plot
+
+from matplotlib import pyplot as plt
+x = [10,20,30,40,50,60,70,80,90]
+y = [9,8,7,6,5,4,3,2,1]
+z = [1,2,3,4,5,6,7,8,9]
+
+# simple scatter plot
+# plt.scatter(x,y)
+# plt.show()
+
+# plot with extra parameters
+# plt.scatter(x,y,marker="*",c="red",s=100)
+# # plt.bar(x,y)
+# # plt.plot(x,y,color ="pink")
+# plt.title("Scatter Plot")
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")plt.grid(True)
+# plt.show()
+
+# some extra
+plt.scatter(x,y,marker="*",c="red",s=100)
+plt.scatter(x,z,marker=".",c="black",s=100)
+# plt.bar(x,z)
+# plt.plot(x,y,color ="pink")
+plt.title("Scatter Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.grid(True)
+plt.show()
